a7db94aae232daf6e4102a40fc421dcb6e1a0cec
AM\csantos <carlos_santos@trimble.com>
2022-04-12 11:04:09 -0300

    Adding golang client side.

---
+++ golang/go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.17
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	google.golang.org/grpc v1.45.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ golang/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "main/proto"
+)
+
+const (
+	defaultName = "Default : Carlos Henrique"
+)
+
+var (
+	addr = flag.String("addr", "localhost:5000", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
